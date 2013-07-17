692f947dac4c4635a5c5de9e621a859976345463
Adrian Ulrich <adrian@blinkenlights.ch>
2013-07-17 14:57:34 +0200

    very primitive aliases support

--- src/hgmcmd/proxy.go
+++ src/hgmcmd/proxy.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"time"
+	"encoding/json"
 	"libhgms/flickr/png"
 	"libhgms/crypto/aestool"
 )
@@ -12,20 +14,48 @@ import (
 /* Custom HTTP Client, setup done in main() */
 var backendClient *http.Client
 
+type AliasJSON struct {
+	Location string
+	Key string
+}
 
 
 func LaunchProxy(bindAddr string, bindPort string) {
 	tr := &http.Transport{ ResponseHeaderTimeout: 5*time.Second }
 	backendClient = &http.Client{Transport: tr}
 	http.HandleFunc("/_raw/", handleRaw)
-	http.HandleFunc("/", handleAny)
+	
+	http.HandleFunc("/", handleAlias)
 	http.ListenAndServe(":8080", nil) 
 }
 
-func handleAny(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "Welcome to Hyper Global Mega Store\n")
+func handleAlias(w http.ResponseWriter, r *http.Request) {
+	/* fixme: directory traversal */
+	aliasPath := fmt.Sprintf("./_aliases/%s", r.RequestURI)
+	
+	fmt.Printf("GET %s -> %s\n", r.RequestURI, aliasPath)
+	
+	content, err := ioutil.ReadFile(aliasPath);
+	
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		io.WriteString(w, "File not found\n")
+		return
+	}
+	
+	var jsx AliasJSON
+	err = json.Unmarshal([]byte(content), &jsx)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, "corrupted metadata\n");
+		fmt.Printf("json error: %s\n", err)
+		return
+	}
+	
+	target := fmt.Sprintf("http://%s", jsx.Location)
+	serveFullURI(w, r, jsx.Key, target)
 }
+
 
 /*
  * Handles a request in /HOSTNAME/URI format
@@ -33,14 +63,14 @@ func handleAny(w http.ResponseWriter, r *http.Request) {
 func handleRaw(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf(">> %s\n", r.RequestURI)
 	target := fmt.Sprintf("http://%s", r.RequestURI[6:])
-	serveFullURI(w, r, target)
+	serveFullURI(w, r, "wurstsalat", target)
 }
 
 /*
  * Handle request for given targetURI
  */
 
-func serveFullURI(dst http.ResponseWriter, rq *http.Request, startURI string) {
+func serveFullURI(dst http.ResponseWriter, rq *http.Request, key string, startURI string) {
 	
 	currentURI := startURI
 	fmt.Printf("======================================================\n")
@@ -74,7 +104,7 @@ func serveFullURI(dst http.ResponseWriter, rq *http.Request, startURI string) {
 			dst.WriteHeader(backendResp.StatusCode)
 		}
 		
-		aes, _ := aestool.New(pngReader.KeySize, pngReader.BlobSize, []byte("wurstsalat"), pngReader.IV);
+		aes, _ := aestool.New(pngReader.KeySize, pngReader.BlobSize, []byte(key), pngReader.IV);
 		err = aes.DecryptStream(dst, pngReader)
 		
 		backendResp.Body.Close()
